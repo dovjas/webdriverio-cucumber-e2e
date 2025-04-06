106d76db959faf62570182eb303cc9596eb21d58
dovjas <dovydas.jaseliunas@gmail.com>
2025-04-06 16:41:15 +0300

    Add input handling

---
+++ test/features/demo/WebInteractions.feature
@@ -0,0 +1,10 @@
+Feature: Web Interactions
+
+    @demo
+    Scenario Outline: Demo web interactions
+        Given A page is opened
+        When Perform web interactions
+
+        Examples:
+            | TestId | 
+            | web_1     | 
--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -24,3 +24,27 @@ Then(/^URL should match (.*)$/,async function (expectedURL) {
     let url = await browser.getUrl();
     await expect(url).to.equal(expectedURL);
 });
+
+// Web interactions
+Given(/^A page is opened$/,async function () {
+    await browser.url('/inputs');
+    await browser.setTimeout({ implicit:15000,pageLoad:10000 });
+    await browser.maximizeWindow();
+});
+
+When(/^Perform web interactions$/,async function(){
+    // 1.Input box
+    let num =123;
+    let numStr = num.toString();
+
+    let inputEl = await $('[type=number]');
+    await inputEl.setValue(numStr);
+    browser.pause(3000);
+
+    for(let i=0;i<numStr.length;i++){
+        let charStr = numStr.charAt(i);
+        await browser.pause(1000);
+        await browser.keys(charStr);
+    };
+});
+
